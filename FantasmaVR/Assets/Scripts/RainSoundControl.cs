4376c313507cb82d5792cf08c4c6b11878b9e076
Anya Schmidt <57494359+anyasc@users.noreply.github.com>
2021-09-22 08:27:39 -0300

    Tentando fazer o som funcionar
    O som não está iniciando

---
+++ FantasmaVR/Assets/Scripts/RainSoundControl.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainSoundControl : MonoBehaviour
+{
+    FMOD.Studio.EventInstance Rain;
+
+    public bool testEnter = false;
+    public bool testExit = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rain = FMODUnity.RuntimeManager.CreateInstance("event:/Rain");
+        Rain.start();
+        Debug.Log(Rain);
+    }
+
+    public void EnterHouse()
+    {
+        Rain.setParameterByName("Place", 1f);
+    }
+
+    public void Leave()
+    {
+        Rain.setParameterByName("Place", 0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (testEnter)
+        {
+            EnterHouse();
+            testEnter = false;
+        }
+        if (testExit)
+        {
+            Leave();
+            testExit = false;
+        }
+    }
+}
